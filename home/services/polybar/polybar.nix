61ee8be7ba8c72accfa93b05300aa4262d32a4fa
John Cinnamond <john@cinnamond.me.uk>
2024-12-29 17:04:04 +0000

    Move "now-playing" into the nix config
    This means I can also use the stylix colors.

--- home/services/polybar/polybar.nix
+++ home/services/polybar/polybar.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+
+let
+  colors = config.lib.stylix.colors.withHashtag;
+  nowPlayingLocation = "${config.xdg.configHome}/polybar/now-playing";
+in
 {
   services.polybar = {
     enable = true;
@@ -7,5 +12,35 @@
     script = ''
       polybar top &
     '';
+    extraConfig = ''
+      [module/now-playing]
+      type = custom/script
+      format = <label>
+      exec = ${nowPlayingLocation}
+      interval = 1
+    '';
+  };
+  home.file."${nowPlayingLocation}" = {
+    text = ''
+      #!/bin/sh
+
+      playerctl="${pkgs.playerctl}/bin/playerctl"
+      playerctlstatus=$($playerctl status)
+      cut="${pkgs.coreutils}/bin/cut"
+
+      if [[ $playerctlstatus == "" ]]; then
+        echo ""
+        exit 0
+      fi
+
+      if [[ $playerctlstatus == "Playing" ]]; then
+        echo -n "%{F${colors.base0D}}▶ "
+      else 
+        echo -n "%{F${colors.base03}} "
+      fi
+
+      $playerctl metadata --format "{{artist}}: {{title}}" | $cut -c-80
+    '';
+    executable = true;
   };
 }

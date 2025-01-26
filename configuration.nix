054dec0814b0b5e3b71964bd913cac71c8d69934
John Cinnamond <john@cinnamond.me.uk>
2025-01-26 15:46:44 +0000

    Set up automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,12 @@
     fsType = "nfs";
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

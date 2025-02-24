940d6b8efc39e963d8f72042d16d6b53f97ead59
John Cinnamond <john@cinnamond.me.uk>
2025-02-24 19:08:17 +0000

    More tmux tweaking

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -11,14 +11,21 @@ in
       unbind-key C-o
       bind-key C-o select-pane -t :.+
       bind-key r source-file ~/.config/tmux/tmux.conf
+      bind-key \{ copy-mode
+      bind-key \} paste-buffer -p
+      bind-key [ swap-pane -U
+      bind-key ] swap-pane -D
+
       set -g bell-action none
 
-      set -g status-style bg="#${bg0}",fg="#${fg}"
+      set-option -g status-position top
+
+      set -g status-style bg="#${bg}",fg="#${fg}"
       set -g status-left " "
-      set -g status-right "#[fg=#${bg0},bg=#${bg1}] #[fg=#${fg1}]#{session_name} "
-      set -g window-status-current-format "#[fg=#${bg0},bg=#${selection}]#[fg=#${fg},bg=#${selection}] #{window_index}:#{window_name} #[bg=#${selection},fg=#${bg0}]"
-      set -g window-status-format "#[fg=#${fg1},bg=#${bg0}] #{window_index}:#{window_name} "
-      set -g window-status-separator ""
+      set -g status-right "#[fg=#${bg1},bg=#${bg}]#[fg=#${fg1},bg=#${bg1}]#{session_name} "
+      set -g window-status-current-format "#[fg=#${selection},bg=#${bg}]#[fg=#${fg},bg=#${selection}]#{window_index}:#{window_name}#[bg=#${bg},fg=#${selection}]"
+      set -g window-status-format "#[fg=#${fg1},bg=#${bg}] #{window_index}:#{window_name} "
+      set -g window-status-separator " "
 
       set -g pane-active-border-style fg=#${selection}
 

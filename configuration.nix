c285a1b684369a699fdcbde7641888b008711bb0
John Cinnamond <john@cinnamond.me.uk>
2025-03-25 09:48:50 +0000

    Trying a different nvidia driver

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@
     modesetting.enable = true;
     powerManagement.enable = true; # change this if it breaks sleep/suspend
     powerManagement.finegrained = false; # only works on Turing or newer
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };

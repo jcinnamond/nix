0f1dcdc78ccf0486a2e753a8532fa0721fe36fbb
John Cinnamond <john@cinnamond.me.uk>
2025-07-29 09:15:06 +0100

    Don't automount the NAS

--- configuration.nix
+++ configuration.nix
@@ -148,6 +148,11 @@
   fileSystems."/mnt/synology" = {
     device = "synology:/volume1/storage";
     fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=600"
+    ];
   };
 
   nix.gc = {

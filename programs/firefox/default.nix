e5adb824f0f0c32f7d899c4619fc12b0e1f61fd4
John Cinnamond <john@cinnamond.me.uk>
2025-08-22 09:01:34 +0100

    (firefox) add home manager options search

--- programs/firefox/default.nix
+++ programs/firefox/default.nix
@@ -45,6 +45,27 @@
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@np" ];
           };
+          home-manager-options = {
+            name = "Home Manager Options";
+            urls = [
+              {
+                template = "https://home-manager-options.extranix.com/";
+                params = [
+                  {
+                    name = "release";
+                    value = "master";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@hm" ];
+          };
+
           hoogle = {
             name = "Hoogle";
             urls = [

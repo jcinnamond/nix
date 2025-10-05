a73c9173e5e13af8ce78d31dc83b42974950b4e2
John Cinnamond <john@cinnamond.me.uk>
2025-10-05 11:29:30 +0100

    Tweaking nvidia driver to try to fix restore from suspend

--- hosts/nixie/nvidia.nix
+++ hosts/nixie/nvidia.nix
@@ -4,10 +4,9 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true; # change this if it breaks sleep/suspend
-    powerManagement.finegrained = false; # only works on Turing or newer
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
 }

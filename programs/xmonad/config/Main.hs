406023aad1e968eba090638c256ae6bf44dd5887
John Cinnamond <john@cinnamond.me.uk>
2025-12-28 12:11:27 +0000

    (xmonad) add quake-style popup term using named scratchpads

--- programs/xmonad/config/Main.hs
+++ programs/xmonad/config/Main.hs
@@ -7,12 +7,13 @@ import Data.Map qualified as M
 import Nix qualified
 import System.Exit (exitSuccess)
 import Volume qualified
-import XMonad (Button, ButtonMask, ChangeLayout (..), IncMasterN (..), KeyMask, KeySym, LayoutClass (..), ManageHook, Query, Window, X, XConfig (..), button1, button3, className, composeAll, controlMask, doF, doShift, focus, io, kill, mod1Mask, mod4Mask, mouseMoveWindow, mouseResizeWindow, noModMask, runQuery, sendMessage, shiftMask, spawn, stringProperty, stringToKeysym, title, windows, withFocused, xK_Escape, xK_Left, xK_Right, xK_Tab, xK_a, xK_comma, xK_e, xK_equal, xK_i, xK_k, xK_l, xK_m, xK_n, xK_period, xK_q, xK_r, xK_s, xK_semicolon, xK_space, xK_t, xK_z, xmonad, (-->), (.|.), (<+>), (=?), (|||))
+import XMonad (Button, ButtonMask, ChangeLayout (..), IncMasterN (..), KeyMask, KeySym, LayoutClass (..), ManageHook, Query, Window, X, XConfig (..), button1, button3, className, composeAll, controlMask, doF, doFloat, doShift, focus, io, kill, mod1Mask, mod4Mask, mouseMoveWindow, mouseResizeWindow, noModMask, runQuery, sendMessage, shiftMask, spawn, stringProperty, stringToKeysym, title, windows, withFocused, xK_Escape, xK_Left, xK_Right, xK_Tab, xK_a, xK_comma, xK_e, xK_equal, xK_h, xK_i, xK_k, xK_l, xK_m, xK_n, xK_period, xK_q, xK_r, xK_s, xK_semicolon, xK_space, xK_t, xK_z, xmonad, (-->), (.|.), (<+>), (=?), (|||))
 import XMonad.Actions.CopyWindow (copyToAll)
 import XMonad.Actions.PerWorkspaceKeys (bindOn)
 import XMonad.Actions.WindowGo (raiseNext)
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (avoidStruts, docks)
+import XMonad.Hooks.ManageHelpers (doRectFloat)
 import XMonad.Layout (Tall (..))
 import XMonad.Layout.BoringWindows (boringWindows, focusMaster, focusUp)
 import XMonad.Layout.CenterMainFluid (CenterMainFluid (..))
@@ -30,35 +31,41 @@ import XMonad.Layout.Spacing (Border (..), spacingRaw)
 import XMonad.Layout.Tabbed (simpleTabbed)
 import XMonad.Layout.WindowNavigation (windowNavigation)
 import XMonad.StackSet qualified as W
+import XMonad.Util.NamedScratchpad (NamedScratchpad (..), customFloating, defaultFloating, namedScratchpadAction)
 
 main :: IO ()
 main = xmonad $ docks $ ewmh $ myConfig def
 
 myConfig c =
-  theme
-    c
-      { keys = myKeys
-      , mouseBindings = myBindings
-      , workspaces = myWorkspaces
-      , layoutHook = myLayout
-      , manageHook = myManageHook <+> manageHook def
-      }
+    theme
+        c
+            { keys = myKeys
+            , mouseBindings = myBindings
+            , workspaces = myWorkspaces
+            , layoutHook = myLayout
+            , manageHook = myManageHook <+> manageHook def
+            }
 
 myManageHook :: ManageHook
 myManageHook =
-  composeAll
-    [ isStreaming --> doShift "streaming"
-    , className =? "Spotify" --> doShift "music"
-    , className =? "YouTube Music Desktop App" --> doShift "music"
-    , className =? "Signal" --> doShift "chat"
-    , className =? "zoom" --> doShift "chat"
-    , stringProperty "WM_WINDOW_ROLE" =? "browser" --> doShift "web"
-    , title =? "Picture-in-Picture" --> doF copyToAll
-    , title =? "Picture-in-picture" --> doF copyToAll
-    ]
+    composeAll
+        [ isStreaming --> doShift "streaming"
+        , className =? "Spotify" --> doShift "music"
+        , className =? "YouTube Music Desktop App" --> doShift "music"
+        , className =? "Signal" --> doShift "chat"
+        , className =? "zoom" --> doShift "chat"
+        , stringProperty "WM_WINDOW_ROLE" =? "browser" --> doShift "web"
+        , title =? "Picture-in-Picture" --> doF copyToAll
+        , title =? "Picture-in-picture" --> doF copyToAll
+        , className =? "quake" --> doRectFloat (W.RationalRect (1 / 4) (1 / 8) (1 / 2) (5 / 8))
+        ]
 
 myWorkspaces :: [String]
 myWorkspaces = ["dev", "web", "x", "chat", "y", "streaming", "music"]
+
+scratchpads =
+    [ NS "term" "kitty --class=quake" (className =? "quake") defaultFloating
+    ]
 
 myKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
 myKeys _ = M.fromList $ wmKeys <> workspaceKeys myWorkspaces
@@ -68,95 +75,97 @@ xmod = mod4Mask .|. mod1Mask .|. controlMask
 
 wmKeys :: [((KeyMask, KeySym), X ())]
 wmKeys =
-  [ -- Launchers
-    ((xmod, xK_space), spawn "rofi -show drun -show-icons")
-  , -- Manage and navigate windows
-    ((mod1Mask, xK_Tab), bindOn [("streaming", raiseNext isStreaming), ("", focusUp)])
-  , ((xmod, xK_equal), focusMaster)
-  , ((xmod, xK_Left), windows W.swapUp)
-  , ((xmod, xK_Right), windows W.swapDown)
-  , ((xmod, xK_Escape), windows W.swapMaster)
-  , ((xmod, xK_m), withFocused (sendMessage . maximizeRestore))
-  , ((xmod, xK_t), withFocused $ windows . W.sink)
-  , ((xmod, xK_k), kill)
-  , ((xmod, xK_l), sendMessage NextLayout)
-  , ((xmod, xK_period), windows W.swapMaster)
-  , ((xmod, xK_comma), sendMessage (IncMasterN 1))
-  , ((xmod, xK_semicolon), sendMessage (IncMasterN (-1)))
-  , -- System control
-    ((xmod, xK_z), spawn "systemctl suspend")
-  , ((xmod .|. shiftMask, xK_q), io exitSuccess)
-  , -- Multimedia keys
-    ((noModMask, stringToKeysym "XF86AudioPlay"), spawn "playerctl play-pause")
-  , ((noModMask, stringToKeysym "XF86AudioPrev"), spawn "playerctl previous")
-  , ((noModMask, stringToKeysym "XF86AudioNext"), spawn "playerctl next")
-  , ((noModMask, stringToKeysym "XF86AudioStop"), spawn "playerctl stop")
-  , ((noModMask, stringToKeysym "XF86AudioRaiseVolume"), spawn $ Volume.increase 2)
-  , ((noModMask, stringToKeysym "XF86AudioLowerVolume"), spawn $ Volume.decrease 2)
-  , ((noModMask, stringToKeysym "XF86AudioMute"), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-  , -- Workspaces
-    ((xmod, xK_a), (windows . W.greedyView) "dev")
-  , ((xmod .|. shiftMask, xK_a), (windows . W.shift) "dev")
-  ]
+    [ -- Launchers
+      ((xmod, xK_space), spawn "rofi -show drun -show-icons")
+    , -- Manage and navigate windows
+      ((mod1Mask, xK_Tab), bindOn [("streaming", raiseNext isStreaming), ("", focusUp)])
+    , ((xmod, xK_equal), focusMaster)
+    , ((xmod, xK_Left), windows W.swapUp)
+    , ((xmod, xK_Right), windows W.swapDown)
+    , ((xmod, xK_Escape), windows W.swapMaster)
+    , ((xmod, xK_m), withFocused (sendMessage . maximizeRestore))
+    , ((xmod, xK_t), withFocused $ windows . W.sink)
+    , ((xmod, xK_k), kill)
+    , ((xmod, xK_l), sendMessage NextLayout)
+    , ((xmod, xK_period), windows W.swapMaster)
+    , ((xmod, xK_comma), sendMessage (IncMasterN 1))
+    , ((xmod, xK_semicolon), sendMessage (IncMasterN (-1)))
+    , -- System control
+      ((xmod, xK_z), spawn "systemctl suspend")
+    , ((xmod .|. shiftMask, xK_q), io exitSuccess)
+    , -- Multimedia keys
+      ((noModMask, stringToKeysym "XF86AudioPlay"), spawn "playerctl play-pause")
+    , ((noModMask, stringToKeysym "XF86AudioPrev"), spawn "playerctl previous")
+    , ((noModMask, stringToKeysym "XF86AudioNext"), spawn "playerctl next")
+    , ((noModMask, stringToKeysym "XF86AudioStop"), spawn "playerctl stop")
+    , ((noModMask, stringToKeysym "XF86AudioRaiseVolume"), spawn $ Volume.increase 2)
+    , ((noModMask, stringToKeysym "XF86AudioLowerVolume"), spawn $ Volume.decrease 2)
+    , ((noModMask, stringToKeysym "XF86AudioMute"), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+    , -- scratchpads
+      ((xmod, xK_h), namedScratchpadAction scratchpads "term")
+    , -- Workspaces
+      ((xmod, xK_a), (windows . W.greedyView) "dev")
+    , ((xmod .|. shiftMask, xK_a), (windows . W.shift) "dev")
+    ]
 
 workspaceKeys :: [String] -> [((KeyMask, KeySym), X ())]
 workspaceKeys ws =
-  let homerow = [xK_a, xK_r, xK_s, xK_t, xK_n, xK_e, xK_i]
-   in [((xmod, k), (windows . W.greedyView) w) | (k, w) <- zip homerow ws]
-        <> [((xmod .|. shiftMask, k), (windows . W.shift) w) | (k, w) <- zip homerow ws]
+    let homerow = [xK_a, xK_r, xK_s, xK_t, xK_n, xK_e, xK_i]
+     in [((xmod, k), (windows . W.greedyView) w) | (k, w) <- zip homerow ws]
+            <> [((xmod .|. shiftMask, k), (windows . W.shift) w) | (k, w) <- zip homerow ws]
 
 myBindings :: XConfig l -> M.Map (ButtonMask, Button) (Window -> X ())
 myBindings _ =
-  M.fromList
-    [ ((mod4Mask, button1), \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)
-    , ((mod4Mask, button3), \w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)
-    ]
+    M.fromList
+        [ ((mod4Mask, button1), \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)
+        , ((mod4Mask, button3), \w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)
+        ]
 
 theme :: XConfig a -> XConfig a
 theme config =
-  config
-    { borderWidth = 2
-    , focusedBorderColor = Nix.focusedBorderColor
-    , normalBorderColor = Nix.normalBorderColor
-    }
+    config
+        { borderWidth = 2
+        , focusedBorderColor = Nix.focusedBorderColor
+        , normalBorderColor = Nix.normalBorderColor
+        }
 
 myLayout = windowNavigation $ avoidStruts $ maximizeWithPadding 0 $ spaceWindows $ boringWindows layouts
- where
-  layouts =
-    streaming
-      ( (centerSingle . handlePopout) (Tall 1 (1 / 10) (7 / 10))
-          ||| (centerSingle . handlePopout) (CenterMainFluid 1 (1 / 10) (1 / 2))
-          ||| (centerSingle . handlePopout) (Tall 1 (1 / 100) (1 / 2))
-      )
-  spaceWindows = spacingRaw True (Border 0 0 0 0) False (Border 5 5 5 5) True
+  where
+    layouts =
+        streaming
+            ( (centerSingle . handlePopout) (Tall 1 (1 / 10) (7 / 10))
+                ||| (centerSingle . handlePopout) (CenterMainFluid 1 (1 / 10) (1 / 2))
+                ||| (centerSingle . handlePopout) (Tall 1 (1 / 100) (1 / 2))
+            )
+    spaceWindows = spacingRaw True (Border 0 0 0 0) False (Border 5 5 5 5) True
 
-  centerSingle = centeredIfSingle 0.7 0.9
+    centerSingle = centeredIfSingle 0.7 0.9
 
-  streaming =
-    let castingWidth = 1920
-        castingHeight = 1080
-        spareX = screenWidth - castingWidth
-        colWidth = spareX `div` 2
-        spareY = screenHeight - castingHeight
-     in onWorkspace "streaming" $
-          layoutP Streaming (absBox colWidth (spareY `div` 3) castingWidth castingHeight) Nothing Simplest $
-            layoutR 0.1 0.5 (absBox 0 0 colWidth screenHeight) Nothing simpleTabbed $
-              layoutAll (absBox (screenWidth - colWidth) 0 colWidth screenHeight) Grid
+    streaming =
+        let castingWidth = 1920
+            castingHeight = 1080
+            spareX = screenWidth - castingWidth
+            colWidth = spareX `div` 2
+            spareY = screenHeight - castingHeight
+         in onWorkspace "streaming" $
+                layoutP Streaming (absBox colWidth (spareY `div` 3) castingWidth castingHeight) Nothing Simplest $
+                    layoutR 0.1 0.5 (absBox 0 0 colWidth screenHeight) Nothing simpleTabbed $
+                        layoutAll (absBox (screenWidth - colWidth) 0 colWidth screenHeight) Grid
 
-  screenWidth = 3440
-  screenHeight = 1440
+    screenWidth = 3440
+    screenHeight = 1440
 
 isStreaming :: Query Bool
 isStreaming = do
-  cn <- className
-  pure $ "streaming-" `isPrefixOf` cn
+    cn <- className
+    pure $ "streaming-" `isPrefixOf` cn
 
 data Streaming = Streaming
-  deriving (Show, Read)
+    deriving (Show, Read)
 
 instance Predicate Streaming Window where
-  alwaysTrue _ = Streaming
-  checkPredicate _ = runQuery isStreaming
+    alwaysTrue _ = Streaming
+    checkPredicate _ = runQuery isStreaming
 
 handlePopout :: (LayoutClass l Window) => l Window -> ConditionalLayout (PiP l) l Window
 handlePopout l = conditionalLayout popoutProp (PiP popoutProp 1200 l) l

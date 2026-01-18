41dc9ed919a487add8edf75bcc3060b552dffdb9
John Cinnamond <john@cinnamond.me.uk>
2026-01-18 18:41:46 +0000

    Add some git settings

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -8,6 +8,14 @@
         email = "john@cinnamond.me.uk";
       };
       init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
       alias = {
         c = "commit --patch";
         fixup = "commit --amend --no-edit";
@@ -16,6 +24,5 @@
     ignores = [
       "dist-newstyle"
     ];
-
   };
 }

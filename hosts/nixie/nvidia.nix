8bfe0b6ee00ad9e9437d40c989ea2cba759c6a66
John Cinnamond <john@cinnamond.me.uk>
2026-04-03 17:48:45 +0100

    (nvidia) add option for kernelSuspendNotifier
    Trying to fix suspend/resume, but the beta driver required for
    kernelSuspendNotifier is even worst than stable, and locks up
    immediately on suspend.

--- hosts/nixie/nvidia.nix
+++ hosts/nixie/nvidia.nix
@@ -3,10 +3,12 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true; # change this if it breaks sleep/suspend
+    powerManagement = {
+      enable = true; # change this if it breaks sleep/suspend
+      # kernelSuspendNotifier = true; # only enable for 595 or later (currently only available as the beta driver)
+    };
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
 }
